e691fff38506097461e47ca193492cd9725e22e7
MikeGrainger <63049266+MikeGrainger@users.noreply.github.com>
2020-04-17 15:54:19 +0100

    Add files

---
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Wired Brain Coffee - Shop Info Tool");
+
+		}
+	}
+}
